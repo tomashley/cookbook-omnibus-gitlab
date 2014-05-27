00c015394c3852f73b9f84f21255801025738185
Jacob Vosmaer <contact@jacobvosmaer.nl>
2014-05-27 16:39:06 +0200

    Add defaults for the 'ssl' section

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,5 +3,8 @@ default['omnibus-gitlab']['package']['sha256'] = "42e8224f8aa8689ba80380d036a3b3
 
 default['omnibus-gitlab']['data_bag'] = nil
 
+default['omnibus-gitlab']['ssl']['certificate'] = nil
+default['omnibus-gitlab']['ssl']['private_key'] = nil
+
 default['omnibus-gitlab']['gitlab_rb']['nginx']['ssl_certificate'] = "/etc/gitlab/ssl/nginx.crt"
 default['omnibus-gitlab']['gitlab_rb']['nginx']['ssl_certificate_key'] = "/etc/gitlab/ssl/nginx.key"
