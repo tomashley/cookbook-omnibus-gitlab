c4dd53cdcf65b452d877a832e81a07dd6a6302f5
Jacob Vosmaer <contact@jacobvosmaer.nl>
2014-06-05 17:31:13 +0200

    Use munin_plugin to render the plugin

--- recipes/munin_sidekiq_memory.rb
+++ recipes/munin_sidekiq_memory.rb
@@ -2,7 +2,6 @@ cookbook_file File.join(node['munin']['basedir'], "plugin-conf.d/omnibus_gitlab_
   notifies :restart, "service[munin-node]"
 end
 
-cookbook_file File.join(node['munin']['plugin_dir'], 'omnibus_gitlab_sidekiq_rss') do
-  mode "0755"
+munin_plugin 'omnibus_gitlab_sidekiq_rss' do
   notifies :restart, "service[munin-node]"
 end
