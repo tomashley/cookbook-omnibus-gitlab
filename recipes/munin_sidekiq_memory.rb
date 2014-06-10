4fef5ad357d17eb9caf112d9fedc42dccf29f2c2
Jacob Vosmaer <contact@jacobvosmaer.nl>
2014-06-10 10:55:21 +0200

    Manually render the sidekiq plugin

--- recipes/munin_sidekiq_memory.rb
+++ recipes/munin_sidekiq_memory.rb
@@ -2,6 +2,11 @@ cookbook_file File.join(node['munin']['basedir'], "plugin-conf.d/omnibus_gitlab_
   notifies :restart, "service[munin-node]"
 end
 
+cookbook_file File.join(node['munin']['plugin_dir'], "omnibus_gitlab_sidekiq_rss") do
+  mode "0755"
+  notifies :restart, "service[munin-node]"
+end
+
 munin_plugin 'omnibus_gitlab_sidekiq_rss' do
   notifies :restart, "service[munin-node]"
 end
