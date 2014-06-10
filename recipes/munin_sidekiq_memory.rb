de41ab47b6a05510aa6e74570abfa345013ea280
Jacob Vosmaer <contact@jacobvosmaer.nl>
2014-06-10 10:53:59 +0200

    Revert "Correctly copy the plugin file from the cookbook"
    This reverts commit 6943bf6e3ad2114c7c668e20e80da1a676344f76.

--- recipes/munin_sidekiq_memory.rb
+++ recipes/munin_sidekiq_memory.rb
@@ -3,6 +3,5 @@ cookbook_file File.join(node['munin']['basedir'], "plugin-conf.d/omnibus_gitlab_
 end
 
 munin_plugin 'omnibus_gitlab_sidekiq_rss' do
-  create_file true
   notifies :restart, "service[munin-node]"
 end
