429e66dc7ba09e52c11fb3c89d42d722c6818fe2
Jacob Vosmaer <contact@jacobvosmaer.nl>
2014-11-04 12:24:04 +0100

    Configure  PostReceive moving average window_size

--- recipes/munin_sidekiq_postreceive.rb
+++ recipes/munin_sidekiq_postreceive.rb
@@ -1,5 +1,6 @@
-cookbook_file File.join(node['munin']['basedir'], "plugin-conf.d/omnibus_gitlab_sidekiq_postreceive.conf") do
+template File.join(node['munin']['basedir'], "plugin-conf.d/omnibus_gitlab_sidekiq_postreceive.conf") do
   notifies :restart, "service[munin-node]"
+  variables node['omnibus-gitlab']['munin_sidekiq_postreceive'].to_hash
 end
 
 cookbook_file File.join(node['munin']['plugin_dir'], "omnibus_gitlab_sidekiq_postreceive") do
