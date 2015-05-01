7e5c9ac662a6a9bd3dcf5d89293dede897247d30
Jacob Vosmaer <contact@jacobvosmaer.nl>
2015-05-01 15:30:01 +0200

    Fix 'search' invocation outside of recipe code

--- libraries/omnibus-gitlab.rb
+++ libraries/omnibus-gitlab.rb
@@ -3,7 +3,7 @@ module OmnibusGitlab
     data_bag_name = node['omnibus-gitlab']['data_bag']
     data_bag_item = node.chef_environment
 
-    if data_bag_name && search(data_bag_name, "id:#{data_bag_item}").any?
+    if data_bag_name && Chef::Search::Query.new.search(data_bag_name, "id:#{data_bag_item}").any?
       Chef::EncryptedDataBagItem.load(data_bag_name, data_bag_item).to_hash
     else
       Hash.new
