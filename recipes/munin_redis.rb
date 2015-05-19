c26aba0684d9ffd96e2687d4e692695731dcdd94
Jeroen Nijhof <jeroen@jeroennijhof.nl>
2015-05-19 18:24:22 +0200

    Munin redis plugin cant handle underscores correct

--- recipes/munin_redis.rb
+++ recipes/munin_redis.rb
@@ -1,13 +1,13 @@
-cookbook_file File.join(node['munin']['basedir'], "plugin-conf.d/omnibus_gitlab_redis.conf") do
+cookbook_file File.join(node['munin']['basedir'], "plugin-conf.d/omnibus-gitlab-redis.conf") do
   notifies :restart, "service[munin-node]"
 end
 
-cookbook_file File.join(node['munin']['plugin_dir'], "omnibus_gitlab_redis") do
+cookbook_file File.join(node['munin']['plugin_dir'], "omnibus-gitlab-redis") do
   mode "0755"
   notifies :restart, "service[munin-node]"
 end
 
-munin_plugin 'redis_' do
-  plugin "redis_socket_var_opt_gitlab_redis_redis.socket"
+munin_plugin 'omnibus-gitlab-redis' do
+  plugin "omnibus-gitlab-redis_socket_var_opt_gitlab_redis_redis.socket"
   notifies :restart, "service[munin-node]"
 end
