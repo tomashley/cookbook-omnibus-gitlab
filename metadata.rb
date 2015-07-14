6f333a56f142b2c6d4ff9359a7487c28ccd86cfa
Marin Jankovski <maxlazio@gmail.com>
2015-07-14 15:19:41 +0200

    Released cookbook v 2.0.0, time for 2.0.1

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'marin@gitlab.com'
 license          'All rights reserved'
 description      'Installs/Configures GitLab using omnibus-gitlab'
 long_description 'Installs/Configures GitLab using omnibus-gitlab'
-version          '0.2.0'
+version          '0.2.1'
 
