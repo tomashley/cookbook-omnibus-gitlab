9b331dfe283c727bf121c7b18410727992b5453a
Job van der Voort <jobvandervoort@gmail.com>
2015-08-22 20:03:26 +0100

    force yes should always upgrade the package, even if downgrade

--- recipes/default.rb
+++ recipes/default.rb
@@ -24,7 +24,7 @@ when 'debian'
 
   package node['omnibus-gitlab']['package']['name'] do
     version node['omnibus-gitlab']['package']['version']
-    options '--force-downgrade'
+    options '--force-yes'
     notifies :run, 'execute[gitlab-ctl reconfigure]'
   end
 when 'rhel'
