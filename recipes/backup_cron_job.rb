7c1d7eb87c4b687ae17e06661dc5e13a16212404
uid0 <me@ianwilson.org>
2016-01-26 11:16:00 -0500

    Updating cron to use cronic to eliminate email bloat on Sundays.

--- recipes/backup_cron_job.rb
+++ recipes/backup_cron_job.rb
@@ -3,6 +3,16 @@
 backup_cron_job = node['omnibus-gitlab']['backup_cron_job']
 options = ''
 
+remote_file '/usr/local/bin/chronic' do
+  source 'http://habilis.net/cronic/cronic'
+  owner 'root'
+  group 'root'
+  checksum '43e257be51b40aa3d6f00aaa89df2a74cebfd35d594f26c9c440e588b4070fc7'
+  mode '0755'
+  action :create_if_missing
+end
+
+
 if backup_cron_job['skip'].any?
   options << " SKIP=#{backup_cron_job['skip'].join(',')}"
 end
@@ -12,7 +22,7 @@ if backup_cron_job['silent']
 end
 
 cron 'GitLab backup' do
-  command "/opt/gitlab/bin/gitlab-rake gitlab:backup:create #{options}"
+  command "/usr/local/bin/chronic /opt/gitlab/bin/gitlab-rake gitlab:backup:create #{options}"
   hour backup_cron_job['hour']
   minute backup_cron_job['minute']
   weekday backup_cron_job['weekday']
